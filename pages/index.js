8501961303ed65950758da30575910e7ac4a810c
Sidi Becker <sidibecker@hotmail.com>
2021-01-25 20:06:48 -0300

    Day #1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../components/Widget'
+import GitHubCorner from '../components/GitHubCorner';
+import QuizBackground from '../components/QuizBackground';
+import Footer from '../components/Footer';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.wrong};
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <GitHubCorner projectUrl={'https://github.com/SidiBecker'} />
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            Teste
+          </Widget.Header>
+          <Widget.Content>
+            Descricao
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            Teste
+          </Widget.Header>
+          <Widget.Content>
+            Descricao
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
